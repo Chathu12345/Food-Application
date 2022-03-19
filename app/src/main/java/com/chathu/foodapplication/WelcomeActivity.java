ff6a062eeffd54e4696fea2ba28a2feeb16dc186
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-03-19 20:34:40 +0530

    Updated

--- app/src/main/java/com/chathu/foodapplication/WelcomeActivity.java
+++ app/src/main/java/com/chathu/foodapplication/WelcomeActivity.java
@@ -2,13 +2,20 @@ package com.chathu.foodapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(WelcomeActivity.this,RegistrationActivity.class));
     }
 }
