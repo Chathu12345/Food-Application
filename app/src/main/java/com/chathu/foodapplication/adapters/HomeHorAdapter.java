f1bed731409aa7404aa314743df72e4c7aac5826
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-03-21 09:29:36 +0530

    Did changes to Adapter

--- app/src/main/java/com/chathu/foodapplication/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/HomeHorAdapter.java
@@ -1,5 +1,6 @@
 package com.chathu.foodapplication.adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,7 +9,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.chathu.foodapplication.R;
+import com.chathu.foodapplication.models.HomeHorModel;
+
+import java.util.List;
+
 public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHolder> {
+
+    Context context;
+    List<HomeHorModel> list;
+
+    public HomeHorAdapter(Context context, List<HomeHorModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -28,10 +43,13 @@ public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHold
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         ImageView imageView;
-        TextView textView;
+        TextView name;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageView = itemView.findViewById(R.id.IvHorizPizza);
+            name = itemView.findViewById(R.id.hor_text);
         }
     }
 }
