2712130712dbe50e7d845ea5b6ea3bed28f8c3f4
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-03-21 08:36:58 +0530

    Add new features

--- app/src/main/java/com/chathu/foodapplication/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/HomeHorAdapter.java
@@ -1,4 +1,37 @@
 package com.chathu.foodapplication.adapters;
 
-public class HomeHorAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
