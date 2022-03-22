084cb02c74ada87cd653921ba13fb67e646d3ea4
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-03-23 00:51:33 +0530

    did changes

--- app/src/main/java/com/chathu/foodapplication/adapters/HomeVerAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/HomeVerAdapter.java
@@ -1,5 +1,47 @@
 package com.chathu.foodapplication.adapters;
 
-public class HomeVerAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chathu.foodapplication.R;
+import com.chathu.foodapplication.models.HomeVerModel;
+
+import java.util.List;
+
+public class HomeVerAdapter extends RecyclerView.Adapter<HomeVerAdapter.ViewHolder> {
+
+    Context context;
+    List<HomeVerModel> list;
+
+    public HomeVerAdapter(Context context, List<HomeVerModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_vertical_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
