43f7e8ccbf2c32f8b002bcd445e1535ea37efb1e
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-03-25 01:41:03 +0530

    Add new features

--- app/src/main/java/com/chathu/foodapplication/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/HomeHorAdapter.java
@@ -1,6 +1,6 @@
 package com.chathu.foodapplication.adapters;
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,20 +8,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.chathu.foodapplication.R;
 import com.chathu.foodapplication.models.HomeHorModel;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHolder> {
 
-    Context context;
-    List<HomeHorModel> list;
+    UpdateVerticalRec updateVerticalRec;
+    Activity activity;
+    ArrayList<HomeHorModel> list;
 
-    public HomeHorAdapter(Context context, List<HomeHorModel> list) {
-        this.context = context;
+    boolean check = true;
+    boolean select = true;
+    int row_index = -1;
+
+    public HomeHorAdapter(UpdateVerticalRec updateVerticalRec, Activity activity, ArrayList<HomeHorModel> list) {
+        this.updateVerticalRec = updateVerticalRec;
+        this.activity = activity;
         this.list = list;
     }
 
@@ -35,6 +42,13 @@ public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHold
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.imageView.setImageResource(list.get(position).getImage());
         holder.name.setText(list.get(position).getName());
+
+        if (check){
+
+            ArrayList<HomeVerAdapter> homeVerAdapters = new ArrayList<>();
+            homeVerModels.add(new HomeVerModel(R.drawable.pizza1,"Pizza","10:00-23:00","4.9","Min - $34"));
+
+        }
     }
 
     @Override
@@ -46,12 +60,14 @@ public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHold
 
         ImageView imageView;
         TextView name;
+        CardView cardView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             imageView = itemView.findViewById(R.id.IvHorizPizza);
             name = itemView.findViewById(R.id.hor_text);
+            cardView = itemView.findViewById(R.id.cardView);
         }
     }
 }
---
+++ app/src/main/java/com/chathu/foodapplication/adapters/UpdateVerticalRec.java
@@ -0,0 +1,8 @@
+package com.chathu.foodapplication.adapters;
+
+import java.util.ArrayList;
+
+public interface UpdateVerticalRec {
+    public void callBack(int position, ArrayList<HomeVerAdapter> list);
+
+}
