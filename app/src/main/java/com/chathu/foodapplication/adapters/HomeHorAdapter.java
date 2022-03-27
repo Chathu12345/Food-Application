53eda38e97318a18d72f72174afc18048b4f2209
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-03-28 01:23:16 +0530

    new changes

--- app/src/main/java/com/chathu/foodapplication/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/HomeHorAdapter.java
@@ -52,6 +52,69 @@ public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHold
             homeVerModels.add(new HomeVerModel(R.drawable.pizza3,"Pizza 3","10:00-23:00","4.9","Min - $34"));
             homeVerModels.add(new HomeVerModel(R.drawable.pizza4,"Pizza 4","10:00-23:00","4.9","Min - $34"));
 
+            updateVerticalRec.callBack(position, homeVerModels);
+            check = false;
+            holder.cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    row_index = position;
+                    notifyDataSetChanged();
+
+                    if (position == 1){
+
+                        ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                        homeVerModels.add(new HomeVerModel(R.drawable.hamburger,"Burger 1","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.burger2,"Burger 2","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.burger4,"Burger 3","10:00-23:00","4.9","Min - $34"));
+
+                        updateVerticalRec.callBack(position,homeVerModels);
+                    }
+                    else if (position == 2){
+
+                        ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                        homeVerModels.add(new HomeVerModel(R.drawable.fries1,"Fries 1","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.fries2,"Fries 2","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.fries3,"Fries 3","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.fries4,"Fries 4","10:00-23:00","4.9","Min - $34"));
+
+                        updateVerticalRec.callBack(position,homeVerModels);
+                    }
+                    else if (position == 3){
+
+                        ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                        homeVerModels.add(new HomeVerModel(R.drawable.icecream1,"Ice Cream 1","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.icecream2,"Ice Cream 2","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.icecream3,"Ice Cream 3","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.icecream4,"Ice Cream 4","10:00-23:00","4.9","Min - $34"));
+
+                        updateVerticalRec.callBack(position,homeVerModels);
+                    }
+                    else if (position == 4){
+
+                        ArrayList<HomeVerModel> homeVerModels = new ArrayList<>();
+                        homeVerModels.add(new HomeVerModel(R.drawable.sandwich1,"Sandwich 1","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.sandwich2,"Sandwich 2","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.sandwich3,"Sandwich 3","10:00-23:00","4.9","Min - $34"));
+                        homeVerModels.add(new HomeVerModel(R.drawable.sandwich4,"Sandwich 4","10:00-23:00","4.9","Min - $34"));
+
+                        updateVerticalRec.callBack(position,homeVerModels);
+                    }
+                }
+            });
+
+            if (select){
+                if (position == 0){
+                    holder.cardView.setBackgroundResource(R.drawable.change_bg);
+                }
+            }
+
+            else {
+                if (row_index == position){
+                    holder.cardView.setBackgroundResource(R.drawable.change_bg);
+                }else {
+                    holder.cardView.setBackgroundResource(R.drawable.default_bg);
+                }
+            }
         }
     }
 
--- app/src/main/java/com/chathu/foodapplication/adapters/UpdateVerticalRec.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/UpdateVerticalRec.java
@@ -1,8 +1,10 @@
 package com.chathu.foodapplication.adapters;
 
+import com.chathu.foodapplication.models.HomeVerModel;
+
 import java.util.ArrayList;
 
 public interface UpdateVerticalRec {
-    public void callBack(int position, ArrayList<HomeVerAdapter> list);
+    public void callBack(int position, ArrayList<HomeVerModel> list);
 
 }
