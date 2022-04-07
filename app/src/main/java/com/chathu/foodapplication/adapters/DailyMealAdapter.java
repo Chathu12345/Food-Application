501b042b5ece8046de3a4eb390be8fac8ed2ca41
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-04-07 22:51:55 +0530

    Did changes

--- app/src/main/java/com/chathu/foodapplication/adapters/DailyMealAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/DailyMealAdapter.java
@@ -1,4 +1,38 @@
 package com.chathu.foodapplication.adapters;
 
-public class  DailyMealAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class  DailyMealAdapter extends RecyclerView.Adapter<DailyMealAdapter.ViewHolder> {
+
+    Context context;
+    List<DailyMealAdapter> list;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class  ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
