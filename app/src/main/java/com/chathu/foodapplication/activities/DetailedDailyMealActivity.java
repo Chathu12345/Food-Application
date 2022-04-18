e1b452839fda7f23283b443960132d7fea75bdfb
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-04-18 10:39:12 +0530

    added new activity

---
+++ app/src/main/java/com/chathu/foodapplication/activities/DetailedDailyMealActivity.java
@@ -0,0 +1,16 @@
+package com.chathu.foodapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chathu.foodapplication.R;
+
+public class DetailedDailyMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_daily_meal);
+    }
+}
