bbbdf89727cda6008e49854a45208c7fdc35d079
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-04-27 00:07:29 +0530

    did changes

--- app/src/main/java/com/chathu/foodapplication/adapters/DetailedDailyAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/DetailedDailyAdapter.java
@@ -1,4 +1,57 @@
 package com.chathu.foodapplication.adapters;
 
-public class DetailedDailyAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chathu.foodapplication.R;
+import com.chathu.foodapplication.models.DetailedDailyModel;
+
+import java.util.List;
+
+public class DetailedDailyAdapter extends RecyclerView.Adapter<DetailedDailyAdapter.ViewHolder> {
+
+    List<DetailedDailyModel> list;
+
+    public DetailedDailyAdapter(List<DetailedDailyModel> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.detailed_daily_meal_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name,price,description,rating,timing;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.detailed_img);
+            price = itemView.findViewById(R.id.detailed_price);
+            name = itemView.findViewById(R.id.detailed_name);
+            description = itemView.findViewById(R.id.detailed_des);
+            rating = itemView.findViewById(R.id.detailed_rating);
+            timing = itemView.findViewById(R.id.detailed_timing);
+        }
+    }
 }
