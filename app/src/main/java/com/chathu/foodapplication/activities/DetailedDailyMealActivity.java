749ab4c1538126a30151a0b54f92373ce58bf503
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-04-28 08:13:20 +0530

    did changes

--- app/src/main/java/com/chathu/foodapplication/activities/DetailedDailyMealActivity.java
+++ app/src/main/java/com/chathu/foodapplication/activities/DetailedDailyMealActivity.java
@@ -1,12 +1,19 @@
 package com.chathu.foodapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.chathu.foodapplication.R;
+import com.chathu.foodapplication.models.DetailedDailyModel;
+
+import java.util.List;
 
 public class DetailedDailyMealActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<DetailedDailyModel> detailedDailyModelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
