3d44b68075e306afd586a34d72057d8add328388
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-04-28 08:38:29 +0530

    did changes

--- app/src/main/java/com/chathu/foodapplication/adapters/DailyMealAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/DailyMealAdapter.java
@@ -45,6 +45,7 @@ public class  DailyMealAdapter extends RecyclerView.Adapter<DailyMealAdapter.Vie
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, DetailedDailyMealActivity.class);
+                intent.putExtra("type",list.get(position).getType());
             }
         });
     }
