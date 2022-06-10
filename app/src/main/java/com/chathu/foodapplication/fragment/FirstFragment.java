064b2c94102badf33897312744e5d9d8d1e011a5
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-06-10 23:40:21 +0530

    did changes

--- app/src/main/java/com/chathu/foodapplication/fragment/FirstFragment.java
+++ app/src/main/java/com/chathu/foodapplication/fragment/FirstFragment.java
@@ -10,21 +10,7 @@ import android.view.ViewGroup;
 
 import com.chathu.foodapplication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FirstFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FirstFragment() {
         // Required empty public constructor
