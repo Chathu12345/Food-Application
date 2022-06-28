e597fd70623e31898fdc38827d764fd847b14437
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-06-28 22:43:54 +0530

    did changes

--- app/src/main/java/com/chathu/foodapplication/models/CartModel.java
+++ app/src/main/java/com/chathu/foodapplication/models/CartModel.java
@@ -1,4 +1,15 @@
 package com.chathu.foodapplication.models;
 
 public class CartModel {
+    int image;
+    String name;
+    String price;
+    String rating;
+
+    public CartModel(int image, String name, String price, String rating) {
+        this.image = image;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
 }
