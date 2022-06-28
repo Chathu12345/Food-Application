b0aafbb832ffbbce9325a2cedfbac853a3f94a27
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-06-28 22:52:19 +0530

    did changes

--- app/src/main/java/com/chathu/foodapplication/models/CartModel.java
+++ app/src/main/java/com/chathu/foodapplication/models/CartModel.java
@@ -12,4 +12,36 @@ public class CartModel {
         this.price = price;
         this.rating = rating;
     }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
 }
