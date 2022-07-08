76322034bf18e133ef877b8c396e21fe730cb4f5
Chathu12345 <chathurikawijesooriya97@gmail.com>
2022-07-08 23:23:22 +0530

    did changes

--- app/src/main/java/com/chathu/foodapplication/adapters/HomeVerAdapter.java
+++ app/src/main/java/com/chathu/foodapplication/adapters/HomeVerAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -58,7 +59,7 @@ public class HomeVerAdapter extends RecyclerView.Adapter<HomeVerAdapter.ViewHold
                 sheetView.findViewById(R.id.add_to_cart).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        
+                        Toast.makeText(context,"",Toast.LENGTH_SHORT).show();
                     }
                 });
             }
